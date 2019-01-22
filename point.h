3106c0cd0c0d8761a7c66e0a6358a2c1900c5133
pulkit goel <goel.pulkit03@gmail.com>
2019-01-22 20:50:14 +0530

    added vector functions

--- point.h
+++ point.h
@@ -32,6 +32,36 @@ Point AddPointVector(const Point& p, const Vector& v){
     return pt;
 }
 
+Vector crossProduct(Point p1, Point p2, Point p3){
+    double u1 = p1.x - p2.x;
+    double u2 = p1.y - p2.y;
+    double u3 = p1.z - p2.z;
+    double v1 = p2.x - p3.x;
+    double v2 = p2.y - p3.y;
+    double v3 = p2.z - p3.z;
+    double uvi = u2 * v3 - v2 * u3;
+    double uvj = v1 * u3 - u1 * v3;
+    double uvk = u1 * v2 - v1 * u2;
+    double norm = sqrt(uvi*uvi+uvj*uvj+uvk*uvk);
+    Vector v(uvi/norm,uvj/norm,uvk/norm);
+    return v;
+}
+
+double dotProduct(Vector p1, Vector p2){
+    double uv = p1.i*p2.i + p1.j*p2.j + p1.k*p2.k;
+    return uv;
+}
+
+Vector getSubtractionVector(Point p2, Point p1){
+    double x = p1.x - p2.x;
+    double y = p1.y - p2.y;
+    double z = p1.z - p2.z;
+    Vector v(x,y,z);
+    return v;
+} 
+
+
+
 class Ray{
     Point source;
     Vector direction;
